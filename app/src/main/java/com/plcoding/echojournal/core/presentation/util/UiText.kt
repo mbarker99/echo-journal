233baa463ffbc4c2e80250f6a034e1bb5c300632
mbrkr <michael.barker1999@gmail.com>
2025-05-31 15:01:20 -0400

    #1 - Add UiText util class.

---
+++ app/src/main/java/com/plcoding/echojournal/core/presentation/util/UiText.kt
@@ -0,0 +1,48 @@
+package com.plcoding.echojournal.core.presentation.util
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+/**
+ * This class is a wrapper class for string resources, so that I can access string resources without context.
+ * I.e. in ViewModels, for testing.
+ */
+@Stable
+sealed interface UiText {
+    data class Dynamic(val value: String): UiText
+
+    @Stable
+    data class StringResource(
+        @StringRes val id: Int,
+        val args: Array<Any> = arrayOf()
+    ): UiText {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as StringResource
+
+            if (id != other.id) return false
+            if (!args.contentEquals(other.args)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = id
+            result = 31 * result + args.contentHashCode()
+            return result
+        }
+    }
+
+    @Composable
+    fun asString(): String {
+        return when(this) {
+            is Dynamic -> value
+            is StringResource -> stringResource(id, *args)
+        }
+    }
+}
