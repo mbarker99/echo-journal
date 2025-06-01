0c95b9ca29257e46468ea5833abc53583c6ea0f5
mbrkr <michael.barker1999@gmail.com>
2025-06-01 16:22:39 -0400

    Add base EchosScreen. Add EchosViewModel. Add EchosAction. Add EchosState. Add EchosScreen empty state UI. Add base floating action button.

---
+++ app/src/main/java/com/plcoding/echojournal/echos/presentation/echos/EchosScreen.kt
@@ -0,0 +1,111 @@
+package com.plcoding.echojournal.echos.presentation.echos
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.plcoding.echojournal.R
+import com.plcoding.echojournal.core.presentation.designsystem.theme.EchoJournalTheme
+import com.plcoding.echojournal.core.presentation.designsystem.theme.bgGradient
+import com.plcoding.echojournal.echos.presentation.echos.components.EchosEmptyBackground
+import com.plcoding.echojournal.echos.presentation.echos.components.EchosTopBar
+import com.plcoding.echojournal.echos.presentation.echos.components.RecordEchoFloatingActionButton
+
+@Composable
+fun EchosRoot(
+    viewModel: EchosViewModel = viewModel()
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    EchosScreen(
+        state = state,
+        onAction = viewModel::onAction
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EchosScreen(
+    state: EchosState,
+    onAction: (EchosAction) -> Unit,
+) {
+    Scaffold(
+        floatingActionButton = {
+            RecordEchoFloatingActionButton(
+                onClick = { onAction(EchosAction.OnFabClick) }
+            )
+        },
+        topBar = {
+            EchosTopBar(
+                onSettingsClick = { onAction(EchosAction.OnSettingsClick) }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = MaterialTheme.colorScheme.bgGradient
+                )
+                .padding(innerPadding)
+        ) {
+            when {
+                state.isLoadingData -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxWidth()
+                            .wrapContentSize(),
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+
+                !state.hasEchosRecorded -> {
+                    EchosEmptyBackground(
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxWidth()
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun EchosScreenPreview() {
+    EchoJournalTheme {
+        EchosScreen(
+            state = EchosState(
+                isLoadingData = false,
+                hasEchosRecorded = false
+            ),
+            onAction = {},
+        )
+    }
+
+}
---
+++ app/src/main/java/com/plcoding/echojournal/echos/presentation/echos/EchosState.kt
@@ -0,0 +1,9 @@
+package com.plcoding.echojournal.echos.presentation.echos
+
+data class EchosState(
+    val hasEchosRecorded: Boolean = false,
+    val hasActiveTopicFilters: Boolean = false,
+    val hasActiveMoodFilters: Boolean = false,
+    val isLoadingData: Boolean = false
+
+)
