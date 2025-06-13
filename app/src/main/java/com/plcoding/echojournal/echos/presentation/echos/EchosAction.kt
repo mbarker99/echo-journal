5c67284610fc436cdae1f41fe1075b477c3d2044
mbrkr <michael.barker1999@gmail.com>
2025-06-13 18:49:44 -0400

    Add EchoFilterRow and functionality.

--- app/src/main/java/com/plcoding/echojournal/echos/presentation/echos/EchosAction.kt
+++ app/src/main/java/com/plcoding/echojournal/echos/presentation/echos/EchosAction.kt
@@ -1,12 +1,17 @@
 package com.plcoding.echojournal.echos.presentation.echos
 
 import com.plcoding.echojournal.echos.presentation.echos.model.EchoFilterChip
+import com.plcoding.echojournal.echos.presentation.model.MoodUi
 
 sealed interface EchosAction {
     data object OnMoodChipClick: EchosAction
+    data object OnDismissMoodDropDown: EchosAction
+    data class OnFilterByMoodClick(val moodUi: MoodUi): EchosAction
     data object OnTopicChipClicked: EchosAction
+    data object OnDismissTopicDropDown: EchosAction
+    data class OnFilterByTopicClick(val topic: String): EchosAction
     data object OnFabClick: EchosAction
     data object OnFabLongPress: EchosAction
     data object OnSettingsClick: EchosAction
-    data class OnRemoveFilters(val filterChip: EchoFilterChip): EchosAction
+    data class OnRemoveFilters(val filterType: EchoFilterChip): EchosAction
 }
--- app/src/main/java/com/plcoding/echojournal/echos/presentation/echos/EchosViewModel.kt
+++ app/src/main/java/com/plcoding/echojournal/echos/presentation/echos/EchosViewModel.kt
@@ -1,37 +1,171 @@
 package com.plcoding.echojournal.echos.presentation.echos
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.echojournal.R
+import com.plcoding.echojournal.core.presentation.designsystem.dropdowns.Selectable
+import com.plcoding.echojournal.core.presentation.util.UiText
+import com.plcoding.echojournal.echos.presentation.echos.model.EchoFilterChip
+import com.plcoding.echojournal.echos.presentation.echos.model.MoodChipContent
+import com.plcoding.echojournal.echos.presentation.model.MoodUi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 
 class EchosViewModel : ViewModel() {
 
+    private var hasLoadedInitialData = false
+
+    private val selectedMoodFilters = MutableStateFlow<List<MoodUi>>(emptyList())
+    private val selectedTopicFilters = MutableStateFlow<List<String>>(emptyList())
+
     private val _state = MutableStateFlow(EchosState())
     val state = _state
+        .onStart {
+            if (!hasLoadedInitialData) {
+                observeFilters()
+                hasLoadedInitialData = true
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000L),
+            initialValue = EchosState()
+        )
 
     fun onAction(action: EchosAction) {
         when (action) {
-            EchosAction.OnFabClick -> {
-
-            }
-
-            EchosAction.OnFabLongPress -> {
-
-            }
+            EchosAction.OnFabClick -> {}
+            EchosAction.OnFabLongPress -> {}
+            EchosAction.OnSettingsClick -> {}
 
             EchosAction.OnMoodChipClick -> {
-
+                _state.update {
+                    it.copy(
+                        selectedEchoFilterChip = EchoFilterChip.MOODS
+                    )
+                }
             }
-
             is EchosAction.OnRemoveFilters -> {
-
+                when (action.filterType) {
+                    EchoFilterChip.MOODS -> selectedMoodFilters.update { emptyList() }
+                    EchoFilterChip.TOPICS -> selectedTopicFilters.update { emptyList() }
+                }
             }
-
             EchosAction.OnTopicChipClicked -> {
-
+                _state.update {
+                     it.copy(
+                         selectedEchoFilterChip = EchoFilterChip.TOPICS
+                     )
+                }
             }
 
-            EchosAction.OnSettingsClick -> {
+            EchosAction.OnDismissTopicDropDown,
+            EchosAction.OnDismissMoodDropDown -> {
+                _state.update { it.copy(selectedEchoFilterChip = null) }
+            }
 
+            is EchosAction.OnFilterByMoodClick -> {
+                toggleMoodFilter(action.moodUi)
+            }
+
+            is EchosAction.OnFilterByTopicClick -> {
+               toggleTopicFilter(action.topic)
+            }
+        }
+    }
+
+    private fun toggleMoodFilter(moodUi: MoodUi) {
+        selectedMoodFilters.update { selectedMoods ->
+            if (moodUi in selectedMoods) {
+                selectedMoods - moodUi
+            } else  {
+                selectedMoods + moodUi
+            }
+        }
+    }
+
+    private fun toggleTopicFilter(topic: String) {
+        selectedTopicFilters.update { selectedTopics ->
+            if (topic in selectedTopics) {
+                selectedTopics - topic
+            } else  {
+                selectedTopics + topic
+            }
+        }
+    }
+
+    private fun observeFilters() {
+        combine(
+            selectedTopicFilters,
+            selectedMoodFilters
+        ) { selectedTopics, selectedMoods ->
+            _state.update { it.copy(
+                topics = it.topics.map{ selectableTopic ->
+                    Selectable(
+                        item = selectableTopic.item,
+                        selected = selectedTopics.contains(selectableTopic.item)
+                    )
+                },
+                moods = MoodUi.entries.map { moodUi ->
+                    Selectable(
+                        item = moodUi,
+                        selected = selectedMoods.contains(moodUi)
+                    )
+                },
+                hasActiveMoodFilters = selectedMoods.isNotEmpty(),
+                hasActiveTopicFilters = selectedTopics.isNotEmpty(),
+                topicChipTitle = selectedTopics.asTopicChipContent(),
+                moodChipContent = selectedMoods.asMoodChipContent()
+            ) }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun List<String>.asTopicChipContent() : UiText {
+        return when (size) {
+            0 -> UiText.StringResource(R.string.all_topics)
+            1 -> UiText.Dynamic(this.first())
+            2 -> UiText.Dynamic("${this.first()}, ${this.last()}")
+            else -> {
+                val extraElementCount = size - 2
+                UiText.Dynamic("${this.first()}, ${this[1]} +$extraElementCount")
+            }
+        }
+    }
+
+    private fun List<MoodUi>.asMoodChipContent(): MoodChipContent {
+        if (this.isEmpty()) {
+            return MoodChipContent()
+        }
+
+        val icons = this.map { it.iconSet.fill }
+        val moodNames = this.map { it.title }
+
+        return when(size) {
+            1 -> MoodChipContent(
+                iconsRes = icons,
+                title = moodNames.first()
+            )
+            2 -> MoodChipContent(
+                iconsRes = icons,
+                title = UiText.Combined(
+                    format = "%s, %s",
+                    uiTexts = moodNames.toTypedArray()
+                )
+            )
+            else -> {
+                val extraElementCount = size - 2
+                MoodChipContent(
+                    iconsRes = icons,
+                    title = UiText.Combined(
+                        format = "%s, %s +$extraElementCount",
+                        uiTexts = moodNames.take(2).toTypedArray()
+                    )
+                )
             }
         }
     }
